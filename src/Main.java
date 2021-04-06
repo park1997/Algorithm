4dbb92036866503dab9b2c2ab377cd50a248ec43
park1997 <iambyunghyun97@gmail.com>
2021-04-06 20:44:58 +0900

    Algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
+
+
 
 public class Main {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
 
-        for(int i = 1; i <=num;i++){
-            for(int j =1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        
+
 
     }
 }
