1a5731a9c86141bb9a4836b5588e3e4e4a6e6d2c
park1997 <iambyunghyun97@gmail.com>
2021-04-12 21:14:02 +0900

    midterm

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,19 @@
-import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        Scanner sc = new Scanner(System.in);
-        int num = 1;
-        for (int i = 0; i < 3; i++) {
-            num *= sc.nextInt();
-        }
-        String a = String.valueOf(num);
-        char b;
+        String myname = "byunghyun";
 
-        for (int j = 0; j < 10; j++) {
-            int cnt = 0;
-            for (int i = 0; i < a.length(); i++) {
-                int temp = a.charAt(i)-48;
-                if (j == temp) {
-                    cnt += 1;
-                }
-            }
-            System.out.println(cnt);
+        char[] array = new char[1024];
+
+        System.out.println(Arrays.toString(array));
+
+        for(int i=0 ; i < myname.length() ; i++) {
+            array[i] = myname.charAt(i);
+            System.out.print(array[i]);
         }
+//         배열의 i번쨰 원소는 내이름의 i번째 문자다!
     }
 
 }
