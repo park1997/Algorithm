ae5535b18cfc8cb4dd5b08528c3c46038cff1a75
park1997 <iambyunghyun97@gmail.com>
2021-04-20 20:49:48 +0900

    midterm

--- src/MyLab7/Main.java
+++ src/MyLab7/Main.java
@@ -19,14 +19,14 @@ class Stack {
     }
     void push(int e) {
         Node node = new Node(e);
-        node.next = head;
-        head = node;
+        node.next = head; // 헤드가 원래 보고있었애가 새로만들어진 노드의 다음값임
+        head = node; // 헤드에 현재 노드 저장
     }
     int top() {
         return head.data;
     }
     void pop() {
-        head = head.next;
+        head = head.next; // 헤드가
     }
     boolean empty() {
         return head == null;
--- src/ex02.java
+++ src/ex02.java
@@ -1,23 +1,35 @@
 import java.util.Scanner;
-
 public class ex02 {
+
+    static int pi2(int n) {
+        int cnt = 0;
+        for (int i = 2; i <= n; i++) {
+            boolean a = true;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    a = false;
+                    break;
+                }
+            }
+            if (a == true) {
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String b = sc.nextLine();
-        char[] a = new char[100];
+        String str = sc.nextLine();
+        char[] stack = new char[100];
         int top = -1;
-
-        for (int i = 0; i < b.length(); i++) {
-            a[++top]= b.charAt(i);
+        for (int i = 0; i < str.length(); i++) {
+            stack[++top] = str.charAt(i); //push하는거
         }
-        while (true) {
-            if (top == -1) {
-                break;
-            }
-            System.out.println(a[top--]);
-
+        while (top > -1) {
+            System.out.println(stack[top--]);
         }
-
 
     }
 }
