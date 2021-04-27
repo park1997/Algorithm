9dadd4ec282a25a0348298c03f3f7e9c9d4274f5
park1997 <iambyunghyun97@gmail.com>
2021-04-27 16:37:58 +0900

    MyLab12
    요세푸스문제

--- src/MyLab12/Main.java
+++ src/MyLab12/Main.java
@@ -1,5 +1,5 @@
 package MyLab12;
-
+// 요세푸스 문제
 import java.util.Scanner;
 
 public class Main {
@@ -10,6 +10,18 @@ public class Main {
         // d. - put k-1 numbers into queue.
         // e. - get a number from queue.
         // f. - print the number
+        int size = n + 1;
+        int[] queue = new int[size];
+        int head = 0, tail = 0;
+        while (head != tail) {
+            for (int i = 1; i < k; i++) {
+                queue[tail] = queue[head];
+                tail = (tail + 1) % size;
+                head = (head + 1) % size;
+            }
+            System.out.println(queue[head]);
+            head = (head + 1) % size;
+        }
 
     }
     public static void main(String[] args) {
