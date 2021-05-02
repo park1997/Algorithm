b802d25ccb831082e4d7f345f470b6f504d21976
park1997 <iambyunghyun97@gmail.com>
2021-05-02 15:02:20 +0900

    Create Bicycle.java

---
+++ src/Tutor/Bicycle.java
@@ -0,0 +1,18 @@
+package Tutor;
+
+public class Bicycle {
+    public String color;
+    public int price;
+
+    public Bicycle() {
+        System.out.println(" Bicycle constructor");
+    }
+    public void run() {
+        System.out.println("-- run --");
+    }
+    public void stop() {
+        System.out.println(" -- stop --");
+    }
+
+
+}
