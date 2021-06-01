62fd34e21aeff2769275d4d188fdbeed74e20104
park1997 <iambyunghyun97@gmail.com>
2021-06-01 14:51:39 +0900

    HW

--- src/HW3/Hw1.java
+++ src/HW3/Hw1.java
@@ -1,16 +1,88 @@
 package HW3;
 
-import java.util.Hashtable;
+import java.util.Arrays;
 import java.util.Scanner;
+class HashTable{
+    String[] keys;
+    Integer[] values;
+    int M;
+    int cnt = 0;
+    HashTable(int m) {
+        keys = new String[m];
+        values = new Integer[m];
+        this.M = m;
+    }
+    // get hash value if specified key.
+    int hash(String key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+    // Store <key, value> pair at specified key space
+    void put(String key, int value) {
+        int h = hash(key);
+        if (key.equals(keys[h])) {
+            cnt += 1;
+        }
+        // 비어있지 않으면 다음칸에 넣도록 함
+        // M 주기로 하기위해 %M 을 함
+        while (keys[h] != null) {
+            h = (h + 1) % M;
+        }
+        keys[h] = key;
+        values[h] = value;
+    }
+    // if key exist, return the value or return null
+    Integer get(String key) {
+        int h = hash(key);
+        // key 값이 null 이 아니면 계속 돌건데
+        while (values[h] != null) {
+            // 키값이 받아온 키값과 같으면 break
+            if (key.equals(keys[h])) {
+                break;
+            }
+            h = (h + 1) % M;
+        }
+        // null이면 못찾았다는 거임
+        if (keys[h] == null) {
+            return null;
+        }
+        // 찾으면 찾은 value를 return
+        return values[h];
+    }
+    void remove(String key) {
+        int h = hash(key);
+        while (values[h] != null) {
+            // null에대해서 함수가 호출이 안되어서 이렇게 조건달아야함
+            if (key.equals(keys[h])) {
+                keys[h] = null;
+                return;
+            }
+            h = (h + 1) & M;
+        }
+    }
+    // check if specified key exist at hash table
+    boolean contains(String key) {
+        int h = hash(key);
+        while (values[h] != null) {
+            if (key.equals(keys[h])) {
+                return true;
+            }
+            h = (h + 1) % M;
+        }
+        return false;
+    }
+
+}
+
 
 public class Hw1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Hashtable ht = new Hashtable();
+
         String str = sc.next();
         int len = str.length() * (str.length() + 1) / 2;
         int cnt = 0;
         String[] arr = new String[len];
+        HashTable ht = new HashTable(5050);
         for (int i = 0; i < str.length(); i++) {
             for (int j = i; j < str.length(); j++) {
                 arr[cnt] = str.substring(i, j + 1);
@@ -18,9 +90,17 @@ public class Hw1 {
             }
 
         }
-        for (String unit : arr) {
-            ht.put(unit, unit);
+//        System.out.println(arr.length+" "+len);
+//        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            ht.put(arr[i], 1);
         }
-        System.out.println(ht.size());
+
+        System.out.println(len-ht.cnt);
+
+
+
+
+
     }
 }
--- src/MyLab17_2/Main.java
+++ src/MyLab17_2/Main.java
@@ -1,14 +1,11 @@
-package MyLab17;
+package MyLab17_2;
 
 import java.util.Scanner;
 
-// hashtable 만들기(1차 조사)
-/*
-key : 존재 , value : 존재 => 실제로 존재
-key = null , value : 존재 => 지워진 값
-key = null, value = null => 비어있는 값
-int로는 안된 참조형 자료변수(Integer)로 바꾸어줘야 null값을 처리 할 수있음
- */
+// hashtable 만들기(2차 조사)
+// 더블 해싱
+// 데이터가 이미 꽉차있는데 더넣게되면 무한루프에 빠지게됨
+
 class HashTable{
     String[] keys;
     Integer[] values;
@@ -22,13 +19,20 @@ class HashTable{
     int hash(String key) {
         return (key.hashCode() & 0x7fffffff) % M;
     }
+
+    // double hasing function
+    int hash2(String key) {
+        // 19번째 줄 M은 13임 근데 우연치않게 여기에 17이아니라 13인경우에는 무한루프를 돌게됨 M보다 작은 값을 해줘야함 그러면 문제가 발생하지 않음 모든 공간을 탐색할 수 있게됨
+        return (key.hashCode() & 0x7fffffff) % 7 + 1; // +1 안하면 우연찮게 0 이되는 경우가 있어서 step을 0만 더하다보니까 현재주소를 계속 찾고 있었던 것임
+    }
     // Store <key, value> pair at specified key space
     void put(String key, int value) {
         int h = hash(key);
+        int step = hash2(key);
         // 비어있지 않으면 다음칸에 넣도록 함
         // M 주기로 하기위해 %M 을 함
         while (keys[h] != null) {
-            h = (h + 1) % M;
+            h = (h + step) % M;
         }
         keys[h] = key;
         values[h] = value;
@@ -36,13 +40,14 @@ class HashTable{
     // if key exist, return the value or return null
     Integer get(String key) {
         int h = hash(key);
+        int step = hash2(key);
         // key 값이 null 이 아니면 계속 돌건데
         while (values[h] != null) {
-            // 키값이 받아온 키값과 같으면 break
+            // 키값이 받아온 키값과 같으 break
             if (key.equals(keys[h])) {
                 break;
             }
-            h = (h + 1) % M;
+            h = (h + step) % M;
         }
         // null이면 못찾았다는 거임
         if (keys[h] == null) {
@@ -53,23 +58,25 @@ class HashTable{
     }
     void remove(String key) {
         int h = hash(key);
+        int step = hash2(key);
         while (values[h] != null) {
             // null에대해서 함수가 호출이 안되어서 이렇게 조건달아야함
             if (key.equals(keys[h])) {
                 keys[h] = null;
                 return;
             }
-            h = (h + 1) & M;
+            h = (h + step) & M;
         }
     }
     // check if specified key exist at hash table
     boolean contains(String key) {
         int h = hash(key);
+        int step = hash2(key);
         while (values[h] != null) {
             if (key.equals(keys[h])) {
                 return true;
             }
-            h = (h + 1) % M;
+            h = (h + step) % M;
         }
         return false;
     }
@@ -79,7 +86,7 @@ class HashTable{
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        HashTable map = new HashTable(449);
+        HashTable map = new HashTable(13);
         map.put("one", 1);
         map.put("two", 2);
         map.put("three", 3);
