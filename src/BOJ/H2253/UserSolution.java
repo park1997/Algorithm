49248e39138baf2773112711dab90aacbd090638
park1997 <iambyunghyun97@gmail.com>
2022-12-08 22:07:04 +0900

    algorithm

--- src/BOJ/H2253/UserSolution.java
+++ src/BOJ/H2253/UserSolution.java
@@ -1,14 +1,29 @@
 package BOJ.H2253;
 
-class UserSolution
-{
-    public void init(int W, int H)
-    {
+import java.util.Arrays;
 
+class UserSolution {
+    static int[][] graph;
+    static int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
+    static int[] dy = {1, 1, 0, -1, -1, -1, 0, 1};
+    static int w;
+    static int h;
+    public void init(int W, int H) {
+        graph = new int[W][H];
+        w = W;
+        h = H;
     }
 
-    public int selectSeat(int mSeatNum)
-    {
+    public int selectSeat(int mSeatNum) {
+        int x = mSeatNum / w;
+        int y = mSeatNum - (x * w) - 1;
+        int dis = 1;
+        while (true) {
+        
+        }
+
+
         return 0;
     }
+
 }
